f956065ef0c39ffaa264128e2548289c95b2febc
Andrey Davidov <davidovandre5@gmail.com>
2024-10-28 00:44:43 +0700

    retro_tapping

--- keymap.c
+++ keymap.c
@@ -1,46 +1,51 @@
-/*
-Copyright 2019 @foostan
-Copyright 2020 Drashna Jaelre <@drashna>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #include QMK_KEYBOARD_H
 #include <stdio.h>
 
+enum LAYERS {
+    BASE,
+    MEDIA,
+    NAV,
+    SYMBOLS,
+    NUMS,
+    FUN
+};
+
+enum KEYS {
+    MEDIA_ESC = LT(MEDIA,KC_ESC),
+    NAV_TAB = LT(NAV,KC_TAB),
+    SYM_ENT = LT(SYMBOLS,KC_ENT),
+    NUM_BSPC = LT(NUMS,KC_BSPC),
+    FUN_DEL = LT(FUN,KC_DEL)
+};
+
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case LT(1,KC_ESC):
-            return 100;
-        case LT(2,KC_SPC):
-            return 100;
-        case LT(3,KC_TAB):
-            return 100;
-        case LT(4,KC_ENT):
-            return 100;
-        case LT(5,KC_BSPC):
-            return 100;
-        case LT(6,KC_DEL):
+        case MEDIA_ESC:
+        case NAV_TAB:
+        case SYM_ENT:
+        case NUM_BSPC:
+        case FUN_DEL:
             return 100;
         default:
             return TAPPING_TERM;
     }
 }
 
+bool get_retro_tapping(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case MEDIA_ESC:
+        case NAV_TAB:
+        case SYM_ENT:
+        case NUM_BSPC:
+        case FUN_DEL:
+            return true;
+        default:
+            return false;
+    }
+}
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  // ---    BASE    ---
-  [0] = LAYOUT_split_3x6_3(
+  [BASE] = LAYOUT_split_3x6_3(
   //|---------------+----------------+----------------+----------------+----------------+----------------|   |----------------+----------------+----------------+----------------+----------------+----------------|
              XXXXXXX,            KC_Q,            KC_W,            KC_E,            KC_R,            KC_T,                KC_Y,            KC_U,            KC_I,            KC_O,            KC_P, KC_LEFT_BRACKET,
   //|---------------+----------------+----------------+----------------+----------------+----------------|   |----------------+----------------+----------------+----------------+----------------+----------------|
@@ -48,12 +53,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|---------------+----------------+----------------+----------------+----------------+----------------|   |----------------+----------------+----------------+----------------+----------------+----------------|
              XXXXXXX,            KC_Z,            KC_X,            KC_C,            KC_V,            KC_B,                KC_N,            KC_M,         KC_COMM,          KC_DOT,         KC_SLSH,KC_RIGHT_BRACKET,
   //|---------------+----------------+----------------+----------------+----------------+----------------|   |----------------+----------------+----------------+----------------+----------------+----------------|
-                                                           LT(1,KC_ESC),    LT(2,KC_SPC),    LT(3,KC_TAB),        LT(4,KC_ENT),   LT(5,KC_BSPC),    LT(6,KC_DEL)
+                                                              MEDIA_ESC,          KC_SPC,         NAV_TAB,             SYM_ENT,        NUM_BSPC,         FUN_DEL
                                                     //|----------------+----------------+----------------|   |----------------+----------------+----------------|
   ),
 
-  // ---    MEDIA   ---
-  [1] = LAYOUT_split_3x6_3(
+  [MEDIA] = LAYOUT_split_3x6_3(
   //|---------------+----------------+----------------+----------------+----------------+----------------|   |----------------+----------------+----------------+----------------+----------------+----------------|
              QK_BOOT,         XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,             XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,
   //|---------------+----------------+----------------+----------------+----------------+----------------|   |----------------+----------------+----------------+----------------+----------------+----------------|
@@ -65,34 +69,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                     //|----------------+----------------+----------------|   |----------------+----------------+----------------|
   ),
 
-  // ---    NAV     ---
-  [2] = LAYOUT_split_3x6_3(
+  [NAV] = LAYOUT_split_3x6_3(
   //|---------------+----------------+----------------+----------------+----------------+----------------|   |----------------+----------------+----------------+----------------+----------------+----------------|
-             XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,       KC_MS_WH_LEFT,   KC_MS_WH_DOWN,     KC_MS_WH_UP,  KC_MS_WH_RIGHT,  XXXXXXX,         XXXXXXX,
+             XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,       KC_MS_WH_LEFT,   KC_MS_WH_DOWN,     KC_MS_WH_UP,  KC_MS_WH_RIGHT,         XXXXXXX,         XXXXXXX,
   //|---------------+----------------+----------------+----------------+----------------+----------------|   |----------------+----------------+----------------+----------------+----------------+----------------|
              XXXXXXX,         KC_LGUI,         KC_LALT,         KC_LCTL,         KC_LSFT,         XXXXXXX,             KC_LEFT,         KC_DOWN,           KC_UP,        KC_RIGHT,         XXXXXXX,    KC_CAPS_LOCK,
   //|---------------+----------------+----------------+----------------+----------------+----------------|   |----------------+----------------+----------------+----------------+----------------+----------------|
              XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,             KC_HOME,    KC_PAGE_DOWN,      KC_PAGE_UP,          KC_END,         XXXXXXX,         XXXXXXX,
   //|---------------+----------------+----------------+----------------+----------------+----------------|   |----------------+----------------+----------------+----------------+----------------+----------------|
-                                                                XXXXXXX,         _______,         XXXXXXX,            KC_ENTER,         KC_BSPC,          KC_DEL
+                                                                XXXXXXX,         XXXXXXX,         _______,            KC_ENTER,         KC_BSPC,          KC_DEL
                                                     //|----------------+----------------+----------------|   |----------------+----------------+----------------|
   ),
 
-  // ---   XXXXXX   ---
-  [3] = LAYOUT_split_3x6_3(
-  //|---------------+----------------+----------------+----------------+----------------+----------------|   |----------------+----------------+----------------+----------------+----------------+----------------|
-             XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,             XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,
-  //|---------------+----------------+----------------+----------------+----------------+----------------|   |----------------+----------------+----------------+----------------+----------------+----------------|
-             XXXXXXX,         KC_LGUI,         KC_LALT,         KC_LCTL,         KC_LSFT,         XXXXXXX,             XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,
-  //|---------------+----------------+----------------+----------------+----------------+----------------|   |----------------+----------------+----------------+----------------+----------------+----------------|
-             XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,             XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,
-  //|---------------+----------------+----------------+----------------+----------------+----------------|   |----------------+----------------+----------------+----------------+----------------+----------------|
-                                                                XXXXXXX,         XXXXXXX,         _______,             XXXXXXX,         XXXXXXX,         XXXXXXX
-                                                    //|----------------+----------------+----------------|   |----------------+----------------+----------------|
-  ),
-
-  // ---   SYMBOLS  ---
-  [4] = LAYOUT_split_3x6_3(
+  [SYMBOLS] = LAYOUT_split_3x6_3(
   //|---------------+----------------+----------------+----------------+----------------+----------------|   |----------------+----------------+----------------+----------------+----------------+----------------|
              XXXXXXX,         KC_HASH,    KC_BACKSLASH,        KC_SLASH,         XXXXXXX,      KC_PERCENT,             XXXXXXX,         XXXXXXX,   KC_LEFT_PAREN,  KC_RIGHT_PAREN,         XXXXXXX,         XXXXXXX,
   //|---------------+----------------+----------------+----------------+----------------+----------------|   |----------------+----------------+----------------+----------------+----------------+----------------|
@@ -104,8 +93,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                     //|----------------+----------------+----------------|   |----------------+----------------+----------------|
   ),
 
-  // ---    NUMS    ---
-  [5] = LAYOUT_split_3x6_3(
+  [NUMS] = LAYOUT_split_3x6_3(
   //|---------------+----------------+----------------+----------------+----------------+----------------|   |----------------+----------------+----------------+----------------+----------------+----------------|
              XXXXXXX,         XXXXXXX,            KC_7,            KC_8,            KC_9,     KC_NUM_LOCK,             XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,
   //|---------------+----------------+----------------+----------------+----------------+----------------|   |----------------+----------------+----------------+----------------+----------------+----------------|
@@ -117,8 +105,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                     //|----------------+----------------+----------------|   |----------------+----------------+----------------|
   ),
 
-  // ---    FUN     ---
-  [6] = LAYOUT_split_3x6_3(
+  [FUN] = LAYOUT_split_3x6_3(
   //|---------------+----------------+----------------+----------------+----------------+----------------|   |----------------+----------------+----------------+----------------+----------------+----------------|
              XXXXXXX,          KC_F12,           KC_F7,           KC_F8,           KC_F9, KC_PRINT_SCREEN,             XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,
   //|---------------+----------------+----------------+----------------+----------------+----------------|   |----------------+----------------+----------------+----------------+----------------+----------------|
